c1a246ddab625399cfcc3b0b9f540e68d3868722
felixpessoa <felixpessoa90@gmail.com>
2021-10-16 12:40:31 -0300

    Crud do Desjejum Da MV

--- src/controler/Principal.java
+++ src/controler/Principal.java
@@ -1,17 +1,12 @@
 package controler;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
-import model.util.JpaUtil;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		EntityManager em = JpaUtil.getEntityManager();
-		EntityTransaction et = em.getTransaction();
-		et.begin();
-		em.close();
+//		EntityManager em = JpaUtil.getEntityManager();
+//		EntityTransaction et = em.getTransaction();
+//		et.begin();
+//		em.close();
 		
 //		Desjejum d1 = new Desjejum();
 //		d1.setLevar("Pão");
